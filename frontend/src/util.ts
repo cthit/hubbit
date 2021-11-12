f598401d1b6d0abdba7cc5c079ace0462c3bb63a
Obmoc <erik.henr00@gmail.com>
2021-11-12 14:40:40 +0100

    Removed unneccesarry spaces

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -125,7 +125,7 @@ export const formatNick = (cid: string, nick: string) => {
       return `👑 ${nick} 🔧`;
     case 'axellars':
       return `😈 ${nick} 😈`;
-    case 'jacobben':  
+    case 'jacobben':
       return `🐉`;
       case 'erhen':
         return `🪢en`; 
